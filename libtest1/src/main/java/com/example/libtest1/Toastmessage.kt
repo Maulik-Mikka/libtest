d6c661b09a9fc9c087856e94d0db496ca859bcb8
LAPTOP-NE60POJM\Phycom <maulik.patel@phycominc.com>
2019-07-30 17:18:04 +0530

    new file

---
+++ libtest1/src/main/java/com/example/libtest1/Toastmessage.kt
@@ -0,0 +1,14 @@
+package com.example.libtest1
+
+import android.content.Context
+import android.widget.Toast
+
+
+object ToasterMessage {
+
+    fun s(c: Context, message: String) {
+
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+
+    }
+}
