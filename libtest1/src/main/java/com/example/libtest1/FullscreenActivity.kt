945ff262999d3ce4b3321934a30c8c55c5eb71f9
LAPTOP-NE60POJM\Phycom <maulik.patel@phycominc.com>
2019-08-01 13:09:48 +0530

    new file

--- libtest1/src/main/java/com/example/libtest1/FullscreenActivity.kt
+++ libtest1/src/main/java/com/example/libtest1/FullscreenActivity.kt
@@ -65,6 +65,7 @@ class FullscreenActivity : AppCompatActivity(),View.OnClickListener {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
+        dummy_button.setOnClickListener(this)
         dummy_button.setOnTouchListener(mDelayHideTouchListener)
     }
 
